742c7e6721b9d46ad94bbfd82fb47f2e8c75b4f8
jpm5718 <jpm5718@g.rit.edu>
2023-03-21 13:12:05 -0400

    added driver file for postresql, and connect method. connects successfully to database

---
+++ App.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App{
+    private final String url = "jdbc:postgresql://localhost/group6";
+    private final String user = "group6";
+    private final String password = "group6";
+
+    public Connection connect(){
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to PostgreSQL successful.");
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.connect();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SWEN-262-COMIX-group6
+
+#### Database Setup
+
+1. Make sure you have pgAdmin installed.
+2. Download the PostreSQL JDBC Driver and add it to the referenced libraries.
+3. In pgAdmin:
+   1. Under Login/Group roles, create a new user titled "group6"
+   2. In the same window, under the definition tab, give it the password "group6"
+   3. In the same window under the privelages tab, make sure "Can login?" is set to on
+   4. Save
+   5. Right click databases and click "Create"
+   6. Create new database titled "group6"
+   7. Save
+4. Make sure to pull from github (it should be up to date)
+5. When ran, it should say connection successful
