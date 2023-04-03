139324524d9c72da7f7f5c5f9c7b5ad6d1a5ca20
DCorcoran8543 <dcorcoran513@gmail.com>
2023-04-03 16:49:01 -0400

    Added publisher and creator objects

--- src/model/comics/ComicBook.java
+++ src/model/comics/ComicBook.java
@@ -14,11 +14,11 @@ public class ComicBook implements Comic {
     private String issue;
     private String title;
     private String varDesc;
-    private String publisher;
     private String releaseDate;
     private String format;
     private String dateAdded;
-    private String creators;
+    private Publisher publisher;
+    private Creators creators;
     private double value;
     private final int id;
 
@@ -27,39 +27,50 @@ public class ComicBook implements Comic {
         issue = attributes.poll();
         title = attributes.poll();
         varDesc = attributes.poll();
-        publisher = attributes.poll();
         releaseDate = attributes.poll();
         format = attributes.poll();
         dateAdded = attributes.poll();
-        creators = attributes.poll();
+        publisher = new Publisher(attributes.poll());
+        creators = new Creators(attributes.poll());
         id = Integer.parseInt(Objects.requireNonNull(attributes.poll()));
         value = 9.99; //base value of comic (assumption)
     }
 
+    @Override
     public String getSeries() { return series; }
 
+    @Override
     public String getIssue() { return issue; }
 
+    @Override
     public String getTitle() { return title; }
 
+    @Override
     public String getVarDesc() { return varDesc; }
 
-    public String getPublisher() { return publisher; }
-
+    @Override
     public String getReleaseDate() { return releaseDate; }
 
+    @Override
     public String getFormat() { return format; }
 
+    @Override
     public String getDateAdded() { return dateAdded; }
 
-    public String getCreators() {
+    @Override
+    public Publisher getPublisher() { return publisher; }
+
+    @Override
+    public Creators getCreators() {
         return creators;
     }
 
+    @Override
     public double getValue() {
         return value;
     }
-    
+
+    @Override
     public int getId() {
         return id;
     }
--- src/model/comics/Publisher.java
+++ src/model/comics/Publisher.java
@@ -1,4 +1,18 @@
 package src.model.comics;
 
 public class Publisher implements SpecialComicAttribute {
+
+    private String name;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
