96fc343cc38b50e36f9bc9f1431844f486b9be8a
DCorcoran8543 <dcorcoran513@gmail.com>
2023-04-03 16:30:02 -0400

    "Strategies, strategies everywhere"

---
+++ src/model/collections/modifyComicType/SignStrategy.java
@@ -0,0 +1,12 @@
+package src.model.collections.modifyComicType;
+
+import src.model.comics.Comic;
+import src.model.comics.SignedComic;
+
+public class SignStrategy implements DecoratorStrategy {
+    @Override
+    public Comic decorate(Comic comic) {
+        comic = new SignedComic(comic);
+        return comic;
+    }
+}
