5693bc2ce2b2edd1f74aeffc046d06201ae201a7
DCorcoran8543 <dcorcoran513@gmail.com>
2023-04-17 19:53:47 -0400

    Signed comic reverted to original state

--- src/model/comics/SignedComic.java
+++ src/model/comics/SignedComic.java
@@ -2,19 +2,17 @@
  * @author Dan Corcoran
  */
 
+
 package src.model.comics;
 
 public class SignedComic extends ComicBookDecorator {
 
-    private double value;
-
     public SignedComic(Comic comic) {
         super(comic);
-        value = comic.getValue() * 1.05;
     }
 
     @Override
     public double getValue() {
-        return value;
+        return comic.getValue() * 1.05;
     }
 }
